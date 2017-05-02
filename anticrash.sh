4b5b01c74e81bef8025fc50b899266f015b8656b
SorBlack <sorblackplus@gmail.com>
2017-05-02 23:43:46 +0430

    Updated :)

--- anticrash.sh
+++ anticrash.sh
@@ -1,12 +1,12 @@
-cd $home/F80
+cd $home/AntiSpamPlus
 figlet Running
 figlet SprCpu Company
 while true; do
-  tmux kill-session -t F80-CLI
-  tmux new-session -d -s F80-CLI "./Cli.sh"
+  tmux kill-session -t AntiSpamPlus-CLI
+  tmux new-session -d -s AntiSpamPlus-CLI "./Cli.sh"
   tmux detach -s Cli.sh
-  tmux kill-session -t F80-API
-  tmux new-session -d -s F80-API "./Api.sh"
+  tmux kill-session -t AntiSpamPlus-API
+  tmux new-session -d -s AntiSpamPlus-API "./Api.sh"
   tmux detach -s Api.sh
   figlet Bots Are Running !
 sleep 100000
